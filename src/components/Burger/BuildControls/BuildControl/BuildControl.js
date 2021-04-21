f6768fa23f2c996e1b097f87ff3d1f447b7fe419
martin vaskoski <martinvaskovski@hotmail.com>
2021-04-21 14:48:22 +0200

    M: Modified Build Control look

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,18 +4,17 @@ import classes from "./BuildControl.css";
 
 const buildControl = (props) => (
   <div className={classes.BuildControl}>
-    <div className={classes.Label}>
-      {props.label}: {props.amount}
-    </div>
+    <div className={classes.Label}>{props.label}</div>
     <button
       className={classes.Less}
       onClick={props.removed}
       disabled={props.disabled}
     >
-      Less
+      -
     </button>
+    {props.amount}
     <button className={classes.More} onClick={props.added}>
-      More
+      +
     </button>
   </div>
 );
